d8206131b30a3af8bd51b56314cb45014559e65c
raquelxdev <raquel.perez.0147@gmail.com>
2024-07-30 12:06:26 -0600

    primera parte

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import conductores.Conductor;
+import conductores.ConductorDeCoche;
+import conductores.ConductorDeMoto;
+
+public class Main {
+    public static void main(String[] args) {
+        ConductorDeCoche conductorCoche = new ConductorDeCoche("Roberto", "C577448", 10);
+        ConductorDeMoto conductorMoto = new ConductorDeMoto("Celeste", "M445757", true);
+
+        System.out.println("Informacion del Conductor de Coche:");
+        conductorCoche.mostrarInformacion();
+
+        System.out.println("\nInformacion del Conductor de Moto:");
+        conductorMoto.mostrarInformacion();
+    }
+}
